f0ea2b8b64519f2c865d7d8955f7328ef3f6f1a0
Juan Lira <juancassiano@hotmail.com>
2024-04-19 19:31:19 -0300

    test: Should return boa tarde test and should return boa noite test

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -6,11 +6,22 @@ import org.junit.jupiter.api.Test;
 public class SaudacaoUtilTest {
 
   @Test
-  public void saudarTeste(){
+  public void deveSaudarBomdiaTest(){
     String saudacao = SaudacaoUtil.saudar(9);
     assertEquals("Bom dia", saudacao);
   }
 
+  @Test
+  public void deveSaudarBoaTardeTest(){
+    String saudacao = SaudacaoUtil.saudar(16);
+    assertEquals("Boa tarde", saudacao);
+  }
+
+  @Test
+  public void deveSaudarBoaNoiteTest(){
+    String saudacao = SaudacaoUtil.saudar(21);
+    assertEquals("Boa noite", saudacao);
+  }
   @Test
   public void deveLancarException(){
     assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
