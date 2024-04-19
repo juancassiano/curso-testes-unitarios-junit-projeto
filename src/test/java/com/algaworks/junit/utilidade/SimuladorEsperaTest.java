ead1e322204aa6d7941edfb2dddfab7d9fed458c
Juan Lira <juancassiano@hotmail.com>
2024-04-19 19:10:32 -0300

    test: How to disable test withe Disabled annotation

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -5,10 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SimuladorEsperaTest {
   @Test
+  @Disabled("Nao é mais aplicável")
   void deveEsperarEnaoDarTimeOut() {
     assertTimeoutPreemptively(Duration.ofSeconds(1), () -> SimuladorEspera.esperar(Duration.ofMillis(10)));
   }
