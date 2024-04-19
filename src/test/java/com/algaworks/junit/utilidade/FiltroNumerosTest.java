4bad14ab5738b6398612366f8d4d85d662499049
Juan Lira <juancassiano@hotmail.com>
2024-04-18 22:39:14 -0300

    test: Create List Test with Assert Iterable Equals

---
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -0,0 +1,20 @@
+package com.algaworks.junit.utilidade;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class FiltroNumerosTest {
+
+  @Test
+  public void deveRetornarNumerosPares(){
+    List<Integer> numeros = Arrays.asList(1,2,3,4);
+    List<Integer> numerosParesEsperados = Arrays.asList(2,4);
+    List<Integer> resultado = FiltroNumeros.numerosPares(numeros);
+
+    assertIterableEquals(numerosParesEsperados, resultado);
+  }
+}
