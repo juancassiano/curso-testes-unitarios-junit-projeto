289dce5de2d38f274173e58dc969b657e7f023f2
Juan Lira <juancassiano@hotmail.com>
2024-04-22 20:37:42 -0300

    test: Creating Nested classes

--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -5,80 +5,92 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ContaBancariaTest {
 
-  @Test
-  void deveLancarIllegalArgumentExceptionSeSaldoForNegativo(){
-    assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null));
+  @Nested
+  class Saque{
+    @Test
+    void deveLancarIllegalArgumentExceptionSeSaqueValorNegativo(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100.0));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(new BigDecimal(-50)));
+    }
+    @Test
+    void deveLancarIllegalArgumentExceptionSeSaqueValorZero(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.ZERO));
+    }
+
+    @Test
+    void deveLancarIllegalArgumentExceptionSeSaqueValorNulo(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null));
+    }
+
+    @Test
+    void deveLancarRuntimeExceptionSeSaqueValorMaiorQueSaldo(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(RuntimeException.class, () -> contaBancaria.saque(new BigDecimal(200)));
+    }
+
+    @Test
+    void deveSacarSeSaldoForIgualAoValorDeSaque(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      contaBancaria.saque(new BigDecimal(100));
+      assertEquals(BigDecimal.ZERO, contaBancaria.saldo());
+    }
   }
 
-  @Test
-  void deveCriarContaBancaria(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertEquals(new BigDecimal(100), contaBancaria.saldo());
+  @Nested
+  class Deposito{
+    @Test
+    void deveLancarIllegalArgumentExceptionSeDepositoForNulo(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(null));
+    }
+  
+    @Test
+    void deveLancarIllegalArgumentExceptionSeDepositoForNegativo(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(new BigDecimal(-20)));
+    }
+  
+    @Test
+    void deveLancarIllegalArgumentExceptionSeDepositoForZero(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(BigDecimal.ZERO));
+    }
   }
 
-  @Test
-  void deveLancarIllegalArgumentExceptionSeSaqueValorNegativo(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100.0));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(new BigDecimal(-50)));
-  }
-  @Test
-  void deveLancarIllegalArgumentExceptionSeSaqueValorZero(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.ZERO));
+  @Nested
+  class Saldo{
+    @Test
+    void deveLancarIllegalArgumentExceptionSeSaldoForNegativo(){
+      assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null));
+    }
+
+    @Test
+    void deveCriarContaBancaria(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      assertEquals(new BigDecimal(100), contaBancaria.saldo());
+    }
+    
+    @Test
+    void deveSacarSeSaldoForMaiorAoValorDeSaque(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      contaBancaria.saque(new BigDecimal(20));
+      assertEquals(new BigDecimal(80), contaBancaria.saldo());
+    }
+
+    @Test
+    void deveDepositarSeDepositoMaiorQueZero(){
+      ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
+      contaBancaria.deposito(new BigDecimal(50));
+      assertEquals(new BigDecimal(150),contaBancaria.saldo());
+    }
   }
 
-  @Test
-  void deveLancarIllegalArgumentExceptionSeSaqueValorNulo(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null));
-  }
-
-  @Test
-  void deveLancarRuntimeExceptionSeSaqueValorMaiorQueSaldo(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(RuntimeException.class, () -> contaBancaria.saque(new BigDecimal(200)));
-  }
-
-  @Test
-  void deveSacarSeSaldoForIgualAoValorDeSaque(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    contaBancaria.saque(new BigDecimal(100));
-    assertEquals(BigDecimal.ZERO, contaBancaria.saldo());
-  }
-
-  @Test
-  void deveSacarSeSaldoForMaiorAoValorDeSaque(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    contaBancaria.saque(new BigDecimal(20));
-    assertEquals(new BigDecimal(80), contaBancaria.saldo());
-  }
-
-  @Test
-  void deveLancarIllegalArgumentExceptionSeDepositoForNulo(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(null));
-  }
-
-  @Test
-  void deveLancarIllegalArgumentExceptionSeDepositoForNegativo(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(new BigDecimal(-20)));
-  }
-
-  @Test
-  void deveLancarIllegalArgumentExceptionSeDepositoForZero(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(BigDecimal.ZERO));
-  }
-
-  @Test
-  void deveDepositarSeDepositoMaiorQueZero(){
-    ContaBancaria contaBancaria = new ContaBancaria(new BigDecimal(100));
-    contaBancaria.deposito(new BigDecimal(50));
-    assertEquals(new BigDecimal(150),contaBancaria.saldo());
-  }
+  
 }
